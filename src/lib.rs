ed68b4800d94a82419b47cde7ed23643f0b56c5b
Eric Roshan-Eisner <edre@google.com>
2022-11-04 21:13:24 -0700

    Add a frontpage to rustdoc, and give it the simplest example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,75 @@
-extern crate rand;
+//! The `minimax` library provides interfaces for defining two-player
+//! perfect-knowledge games, and strategies for choosing moves.
+//!
+//! Any game can be defined by implementing 2 traits: Game and Move.
+//! ```
+//! use minimax::Strategy;
+//!
+//! // Stateless rules object.
+//! struct TugOfWar;
+//! // State of the game.
+//! #[derive(Clone)]
+//! struct War(i8);
+//! // A move that a player can make.
+//! #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+//! struct Tug(i8);
+//!
+//! impl minimax::Game for TugOfWar {
+//!     type S = War;
+//!     type M = Tug;
+//!
+//!     fn generate_moves(s: &War, moves: &mut Vec<Tug>) {
+//!         moves.push(Tug(-1));
+//!         moves.push(Tug(1));
+//!     }
+//!
+//!     fn get_winner(state: &War) -> Option<minimax::Winner> {
+//!         if state.0 > 9 {
+//!             Some(if state.0 % 2 == 0 {
+//!                 minimax::Winner::PlayerJustMoved
+//!             } else {
+//!                 minimax::Winner::PlayerToMove
+//!             })
+//!         } else if state.0 < 9 {
+//!             Some(if state.0 % 2 == 0 {
+//!                 minimax::Winner::PlayerToMove
+//!             } else {
+//!                 minimax::Winner::PlayerJustMoved
+//!             })
+//!         } else {
+//!             None
+//!         }
+//!     }
+//! }
+//!
+//! impl minimax::Move for Tug {
+//!     type G = TugOfWar;
+//!     fn apply(&self, state: &mut War) {
+//!         state.0 += self.0
+//!     }
+//!     fn undo(&self, state: &mut War) {
+//!         state.0 -= self.0
+//!     }
+//! }
+//!
+//! // To run the search we need an evaluator.
+//! struct Eval;
+//! impl minimax::Evaluator for Eval {
+//!     type G = TugOfWar;
+//!     fn evaluate(&self, state: &War) -> minimax::Evaluation {
+//!         if state.0 % 2 == 0 {
+//!             state.0 as minimax::Evaluation
+//!         } else {
+//!             -state.0 as minimax::Evaluation
+//!         }
+//!     }
+//! }
+//!
+//! // Now we can use a simple Strategy to find a move from the initial state.
+//! let start = War(0);
+//! let mut strategy = minimax::Negamax::new(Eval{}, 3);
+//! let best_move = strategy.choose_move(&start).unwrap();
+//! ```
 
 pub mod interface;
 pub mod strategies;
