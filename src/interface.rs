0b30ae6bf4197490a863a816656be5bed3188de1
Eric Roshan-Eisner <edre@google.com>
2023-03-19 06:49:11 -0700

    Experimental unified Game trait.

--- src/interface.rs
+++ src/interface.rs
@@ -153,6 +153,76 @@ pub trait Game: Sized {
     }
 }
 
+/// An alternate system for defining a game.
+pub trait UniGame {
+    /// The type of the game state.
+    type S;
+    /// The type of game moves.
+    type M: Copy;
+
+    /// Generate moves at the given state.
+    fn generate_moves(state: &Self::S, moves: &mut Vec<Self::M>);
+
+    /// Apply a move to get a new state.
+    /// This method supports two different implementation strategies:
+    ///
+    /// 1) Games with large state that want to update in place.
+    /// ```
+    /// struct BigBoard([u8; 4096]);
+    /// struct BigMove(u16);
+    /// fn apply(state: &mut BigBoard, m: &BigMove) -> Option<BigBoard> {
+    ///     state.0[m.0 as usize] += 1;
+    ///     None
+    /// }
+    /// fn undo(state: &mut BigBoard, m: &BigMove) {
+    ///     state.0[m.0 as usize] -= 1;
+    /// }
+    /// ```
+    ///
+    /// 2) Games with small state that don't want to implement undo.
+    /// ```
+    /// struct SmallBoard(u64);
+    /// struct SmallMove(u8);
+    /// fn apply(state: &mut SmallBoard, m: &SmallMove) -> Option<SmallBoard> {
+    ///     Some(SmallBoard(state.0 | (1<<m.0)))
+    /// }
+    /// ```
+    fn apply(state: &mut Self::S, m: &Self::M) -> Option<Self::S>;
+    fn undo(_state: &mut Self::S, _m: &Self::M) {}
+
+    /// Returns `Some(PlayerJustMoved)` or `Some(PlayerToMove)` if there's a winner,
+    /// `Some(Draw)` if the state is terminal without a winner, and `None` if
+    /// the state is non-terminal.
+    fn get_winner(state: &Self::S) -> Option<Winner>;
+
+    /// Hash of the game state.
+    /// Expected to be pre-calculated and cheaply updated with each apply.
+    fn zobrist_hash(_state: &Self::S) -> u64 {
+        unimplemented!("game has not implemented zobrist hash");
+    }
+
+    /// Optional method to return a move that does not change the board state.
+    /// This does not need to be a legal move from this position, but it is
+    /// used in some strategies to reject a position early if even passing gives
+    /// a good position for the opponent.
+    fn null_move(_state: &Self::S) -> Option<Self::M> {
+        None
+    }
+
+    /// Return a human-readable notation for this move in this game state.
+    fn notation(_state: &Self::S, _move: &Self::M) -> Option<String> {
+        None
+    }
+    /// Return a small index for this move for position-independent tables.
+    fn table_index(_: &Self::M) -> u16 {
+        0
+    }
+    /// Maximum index value.
+    fn max_table_index() -> u16 {
+        0
+    }
+}
+
 /// Defines a method of choosing a move for the current player.
 pub trait Strategy<G: Game> {
     fn choose_move(&mut self, state: &G::S) -> Option<G::M>;
